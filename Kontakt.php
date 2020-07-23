af4a49038d36f09406adca874edc2b4398e13c87
Michael Niers <michael.niers@ewetel.net>
2020-07-23 10:43:19 +0200

    updated PHP and Contact form

--- Kontakt.php
+++ Kontakt.php
@@ -2,14 +2,18 @@
     <head></head>
     <body>
         <?php
+
+        if (isset($_POST['senden'])) {
+        $name = $_REQUEST['name'];
         $email = $_REQUEST['email'];
-        $betreff = $_REQUEST['betreff'];
-        $nachricht = $_REQUEST['nachricht'];
+        $subject = $_REQUEST['subject'];
+        $message = $_REQUEST['message'];
         $empfaenger = "michael.niers@ewetel.net";
-        $absender = $email;
 
-        mail("$empfaenger", "$betreff", "$nachricht\n", "FROM: $absender\n");
+        mail("$empfaenger", "$subject", "$message\n", "FROM: $email\n");
+        header("Location: sent.html");
 
+        }
         ?>
     </body>
 </html>
